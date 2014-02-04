fde1f9ba8dc91fd4408334e9a52eb3fdfc1d534a
Dragon and Steve <dragon@scraperwiki.com>
2014-02-04 12:09:11 +0000

    finally nailed 'this' issue

--- longrun.js
+++ longrun.js
@@ -15,17 +15,20 @@ LongRun = function() {
   var _invoke = function () {window.alert("nope nope nope _invoke")}
 
   var get_one_state = function(name) {
-    var raw_state = this._invoke(["~/bin/this.state", name]);
+    console.log("get_one "+name)
+    console.log("get_one_this"+this)
+    var raw_state = this._invoke(["~/bin/longrun.state", name]);
     // TODO: make the above async, but keep below not async.
     var state = $.parseJSON(raw_state);
     if (state['status'] === 'success') {
-	this._success_functions[name](state['msg'])
+	this._success_functions[name](name, state['msg'])
     } else if (state['status'] === 'failure') {
-	this._failure_functions[name](state['msg'])
+	this._failure_functions[name](name, state['msg'])
     } else if (state['status'] === 'running') {
-	this._poll_functions[name](state['msg'])
+	//this._poll_functions[name](name, state['msg'])
+        this._poll(name)
     } else if (state['status'] === 'killed') {
-	this._failure_functions[name](state['msg'])
+	this._failure_functions[name](name, state['msg'])
     } else {
 	console.log("Didn't know what to do with status "+status)
     }
@@ -38,8 +41,17 @@ LongRun = function() {
       }
   }
 
+  var _poll = function(name, msg) {
+    console.log("_poll:"+name+" "+msg)
+    console.log("_poll this"+this)
+    var that = this
+    window.setTimeout(function(name){that.get_one_state(name)}, 100, name)
+  }
+
   this.register = register
   this._invoke = _invoke
   this.get_one_state = get_one_state
   this.get_state = get_state
+  this._poll = _poll
+
 }
--- tests/test.basic.js
+++ tests/test.basic.js
@@ -1,3 +1,8 @@
+var never_run = function(msg) {
+  console.log("never_run: "+msg)
+  never_run.should.not.be.ok;  // you should never run this function
+}
+
 describe("Longrun", function(){
   LongRun.should.be.ok;
 
@@ -30,20 +35,16 @@ describe("Longrun", function(){
     it("correctly invokes commands and gets output", sinon.test(function () {
       var longrun = new LongRun()
       longrun._invoke = function(commands){return JSON.stringify({"status": commands[1], "msg": commands[1]})}
-      var never_run = function(msg) {
-	console.log("never_run: "+msg)
-	never_run.should.not.be.ok;  // you should never run this function
-      }
 
       var collect_done = []
-
-      done = function(msg) {
-	  collect_done.push(msg)
+      var done = function(name, msg) {
+        collect_done.push(msg)
       }
+
 
       var fail = done;
       var poll = done;
-      die = done;
+      var die = done;
 
       longrun.register('success', done, never_run, never_run);
       longrun.register('failure', never_run, fail, never_run);
@@ -56,8 +57,40 @@ describe("Longrun", function(){
       }
 
       longrun.get_state()
-      collect_done.should.be.eql(['success', 'failure', 'running', 'killed'])
+      //collect_done.should.be.eql(['success', 'failure', 'running', 'killed'])
+      collect_done.should.be.eql(['success', 'failure', 'killed'])
     }))
   })
 
+  describe("_poll", function(){
+    it("polls repeatedly", function(){
+      var longrun = new LongRun()
+      var i = 0
+      var stub_invoke = function(msg){
+        i++;
+        console.log("Stub_invoke "+i)
+        if (i>4) {
+            this.should.not.be.ok
+        } else if (i==4) {
+            return '{"status":"success", "msg":"ok"}'
+        } else {
+            return '{"status":"running", "msg":"chug"}'
+        }
+      }
+      longrun._invoke = stub_invoke;
+      var collect_done = []
+      var done = function(name, msg) {
+          name.should.be.eql("attempt")
+          msg.should.be.eql("ok")
+          window.alert("ok")
+      }
+
+      var fail = done;
+      var success = done;
+      
+      longrun.register("attempt", success, fail, fail)
+      longrun.get_state()
+      
+    })
+  })
 })
